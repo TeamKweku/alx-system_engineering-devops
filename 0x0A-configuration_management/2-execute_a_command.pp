6f84c330e513b3f049b38ea4a3cce402d33bbd15
teamkweku <teamkweku@outlook.com>
2023-09-22 22:12:10 +0000

    manifest that kills a process named killmenow

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# a puppet manifest that kills a process named killmenow
+
+exec {  'killmenow':
+  command => 'pkill killmenow',
+  path    => '/usr/bin:/usr/sbin:/bin:sbin',
+  onlyif  => 'pgrep killmenow',
+}
