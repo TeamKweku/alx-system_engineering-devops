89a9a1cf3ff5b967e0b23fe01483271c02ae1b9e
teamkweku <teamkweku@outlook.com>
2023-10-03 08:23:18 +0000

    fix of puppet code not working on ubuntu 16.04

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,55 +1,35 @@
-# Class: nginx_config
-#
-# This class manages the configuration of the Nginx web server
-# setting custom headers
-#
+# setup a new ubuntu server with nginx and custom HTTP header
 
-class nginx_config {
-
-  package { 'nginx':
-    ensure => 'installed',
-  }
-
-  file { '/var/www/html':
-    ensure => 'directory',
-  }
-
-  file { '/var/www/html/index.html':
-    content => 'Hello World!\n',
-  }
-
-  file { '/var/www/html/404.html':
-    content => "Ceci n'est pas une page\n",
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => 'file',
-    content => "
-      server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        add_header X-Served-By '${facts['networking']['hostname']}';
-        root /var/www/html;
-        index index.html index.htm;
-
-        location /redirect_me {
-          return 301 https://www.youtube.com/watch?v=xJJsoquu70o/;
-        }
-
-        error_page 404 /404.html;
-        location /404 {
-          root /var/www/html;
-          internal;
-        }
-      }
-    ",
-  }
-
-  service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => File['/etc/nginx/sites-available/default'],
-  }
+# update system
+exec {  'update system':
+  command => '/usr/bin/apt-get update',
 }
 
-include nginx_config
+# install nginx server
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update system']
+}
+
+# create custom index.html
+file {  '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# redirect 301 Moved Permanently
+exec {  'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=xJJsoquu70o/ permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+# Add custom HTTP headers 
+exec {  'HTTP header':
+  command  =>  'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => 'shell'
+}
+
+# start nginx servcie
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
