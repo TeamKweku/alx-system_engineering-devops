0b784aede29e1ec7dec4d85a673442a5498564b2
teamkweku <teamkweku@outlook.com>
2023-11-07 18:17:55 +0000

    passing params

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,20 +5,21 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=""):
     """Method that queries the reddit API for hot topics"""
-    url = "https://www.reddit.com/r/{}" \
-        "/hot.json?after={}".format(subreddit, after)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit)
     headers = {"User-Agent": "Custom/1.0"}
+    params = {"after": after}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        children = data.get("data").get("children")
+        children = data.get('data').get('children')
         for entry in children:
             hot_list.append(entry.get("data").get("title"))
 
-        after = data.get("data").get("after")
+        after = data.get('data').get('after')
         if after:
             recurse(subreddit, hot_list, after)
         return hot_list
