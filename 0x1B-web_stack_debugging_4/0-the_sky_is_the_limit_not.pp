072044fbf2eafd3a2e9e77eb6ef0fc049358b1d9
teamkweku <teamkweku@outlook.com>
2023-11-13 08:35:57 +0000

    puppet manifest that fixes nginx ulimit rate

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fixex error on concurrent request to an nginx server
+exec { 'nginx ulimit fix':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
